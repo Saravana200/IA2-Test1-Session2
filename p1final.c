a334155199fafc434019ccc214603d1196052157
Saravana200 <dhruvassaravana@gmail.com>
2022-03-31 13:37:34 +0530

    Update p1final.c

--- p1final.c
+++ p1final.c
@@ -2,7 +2,7 @@
 void input(float *base, float *height)
 {
   printf("enter the base and height\n");
-  scanf("%f %f",&*base,&*height);
+  scanf("%f %f",base,height);
 }
 void find_area(float base, float height, float *area)
 {
